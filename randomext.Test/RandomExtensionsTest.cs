76894a538accf721741978a1ad66ca50b3c18c3e
James Meyer <jamesjmeyer210@gmail.com>
2023-11-30 15:02:44 -0700

    Added implementations for all types other than decimal

--- randomext.Test/RandomExtensionsTest.cs
+++ randomext.Test/RandomExtensionsTest.cs
@@ -28,6 +28,13 @@ public class RandomExtensionsTest
         
         Assert.True(bytes.Count(x => x < byte.MaxValue/2) > FortyPercent);
     }
+
+    [Theory]
+    [InlineData(9, 6)]
+    public void NextByte_ThrowsException(byte min, byte max)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _random.NextByte(min, max));
+    }
     
     [Fact]
     public void NextSByte_IsRandom()
@@ -38,8 +45,15 @@ public class RandomExtensionsTest
         Assert.True(bytes.Count(x => x > 0) > FortyPercent);
     }
 
+    [Theory]
+    [InlineData(99, 66)]
+    public void NextSByte_ThrowsException(byte min, byte max)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _random.NextByte(min, max));
+    }
+    
     [Fact]
-    public void NextShort_IsRandom()
+    public void NextInt16_IsRandom()
     {
         var n = new short[Limit];
         for (var i = 0; i < Limit; i++) n[i] = _random.NextInt16();
@@ -47,13 +61,27 @@ public class RandomExtensionsTest
         Assert.True(n.Count(x => x > 0) > FortyPercent);
     }
     
+    [Theory]
+    [InlineData(999, -555)]
+    public void NextInt16_ThrowsException(short min, short max)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _random.NextInt16(min, max));
+    }
+    
     [Fact]
-    public void NextUShort_IsRandom()
+    public void NextUInt16_IsRandom()
     {
         var n = new ushort[Limit];
         for (var i = 0; i < Limit; i++) n[i] = _random.NextUInt16();
         
         Assert.True(n.Count(x => x > 0) > FortyPercent);
+    }
+    
+    [Theory]
+    [InlineData(999, 555)]
+    public void NextUInt16_ThrowsException(ushort min, ushort max)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _random.NextUInt16(min, max));
     }
     
     [Fact]
@@ -65,6 +93,32 @@ public class RandomExtensionsTest
         Assert.True(n.Count(x => x > uint.MaxValue / 2) > FortyPercent);
     }
     
+    [Theory]
+    [InlineData(9999, 5555)]
+    public void NextUInt32_ThrowsException(uint min, uint max)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _random.NextUInt32(min, max));
+    }
+    
+    [Fact]
+    public void NextUInt64_IsRandom()
+    {
+        var n = new ulong[Limit];
+        for (var i = 0; i < Limit; i++) n[i] = _random.NextUInt64();
+        
+        Assert.True(n.Count(x => x > ulong.MaxValue / 2) > FortyPercent);
+    }
+    
+    [Theory]
+    [InlineData(10L, 255L)]
+    public void NextUInt64_IsWithinRange(ulong min, ulong max)
+    {
+        var n = new ulong[Limit];
+        for (var i = 0; i < Limit; i++) n[i] = _random.NextUInt64(min, max);
+
+        Assert.True(n.All(x => x >= min || x <= max));
+    }
+
     [Fact]
     public void NextChar_IsRandom()
     {
--- randomext/RandomExtensions.cs
+++ randomext/RandomExtensions.cs
@@ -4,17 +4,33 @@ public static class RandomExtensions
 {
     public static bool NextBool(this Random random) => random.Next() % 2 == 0;
 
-    public static byte NextByte(this Random random) => (byte)(random.Next() % byte.MaxValue);
+    public static byte NextByte(this Random random, byte min = byte.MinValue, byte max = byte.MaxValue) => 
+        (byte)random.Next(min, max);
 
-    public static sbyte NextSByte(this Random random) => (sbyte)(random.Next() % byte.MaxValue);
+    public static sbyte NextSByte(this Random random, sbyte min = sbyte.MinValue, sbyte max = sbyte.MaxValue) => 
+        (sbyte)random.Next(min, max);
 
-    public static ushort NextUInt16(this Random random) => (ushort)(random.Next() % ushort.MaxValue);
+    public static ushort NextUInt16(this Random random, ushort min = ushort.MinValue, ushort max = ushort.MaxValue) => 
+        (ushort)random.Next(min, max);
 
-    public static short NextInt16(this Random random) => (short)(random.Next() % ushort.MaxValue);
+    public static short NextInt16(this Random random, short min = short.MinValue, short max = short.MaxValue) => 
+        (short)random.Next(min, max);
 
-    public static int NextInt32(this Random random) => random.Next();
+    public static int NextInt32(this Random random, int min = int.MinValue, int max = int.MaxValue) => 
+        random.Next(min, max);
 
-    public static uint NextUInt32(this Random random) => (uint)(random.NextInt64() % uint.MaxValue);
+    public static uint NextUInt32(this Random random, uint min = uint.MinValue, uint max = uint.MaxValue) => 
+        (uint)random.NextInt64(min, max);
 
-    public static char NextChar(this Random random) => (char)random.NextUInt16();
+    public static ulong NextUInt64(this Random random, ulong min = ulong.MinValue, ulong max = ulong.MaxValue)
+    {
+        var bytes = new byte[8];
+        random.NextBytes(bytes);
+        var x = BitConverter.ToUInt64(bytes);
+        if (x < min) return (x + min) % max;
+        return x % max;
+    }
+
+    public static char NextChar(this Random random, char min = char.MinValue, char max = char.MaxValue) => 
+        (char)random.NextUInt16(min, max);
 }
